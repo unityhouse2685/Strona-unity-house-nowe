01f8025bec40184e081007118cafc9fccfe401b3
unityhouse2685 <unity.housegostyn@gmail.com>
2026-05-12 11:51:18 +0200

    Update system.js

--- system.js
+++ system.js
@@ -3,9 +3,10 @@
 // -----------------------------------------
 
 const SUPABASE_URL = "https://ycuogutnwdybdeobowla.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InljdW9ndXRud2R5YmRlb2Jvd2xhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0OTE4NDAsImV4cCI6MjA5NDA2Nzg0MH0.ObkFIknc3Ce5KEmj435lI_8hi1T7E-lnxQuRSicZlPw";
-
+const SUPABASE_KEY = "twój_publiczny_anon_key"; // znajdziesz w Supabase → Project Settings → API → anon public
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+
 
 // -----------------------------------------
 // LOGOWANIE
